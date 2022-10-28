8e782cd2157b29c04b7d8b8939d3dfcb9f00926f
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-10-28 14:27:30 +0200

    problem Q : accepted

---
+++ sheet7 recursion/Q_3n+_1_sequence.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long long squence(int n)
+{
+    if(n==1){
+        return 1;
+    }
+    if(n%2==0){
+        return squence(n/2)+1;
+    }
+    else{
+        return squence(3*n+1)+1;
+    }
+}
+
+int main()
+{
+    long long n;
+    cin >> n;
+    cout << squence(n);
+
+    return 0;
+}
